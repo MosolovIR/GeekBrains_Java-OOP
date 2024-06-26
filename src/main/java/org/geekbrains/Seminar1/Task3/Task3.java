17743b770b80e042d5d8ed5635e3628337bf3943
MosolovIR <mosolovolosom@gmail.com>
2024-06-26 18:12:03 +0300

    Seminar 1 - 24.06.2024

---
+++ src/main/java/org/geekbrains/Seminar1/Task3/Task3.java
@@ -0,0 +1,11 @@
+package org.geekbrains.Seminar1.Task3;
+
+//TODO: Дана строка. Поменять местами ее половины.
+
+public class Task3 {
+    public static void main(String[] args) {
+        String str = "qwerty";
+        String result = str.substring(str.length() / 2) + str.substring(0, str.length() / 2);
+        System.out.println("Result = " + result);
+    }
+}
---
+++ src/main/java/org/geekbrains/Seminar1/Task4/Task4.java
@@ -0,0 +1,41 @@
+package org.geekbrains.Seminar1.Task4;
+
+//TODO : Дан массив целых чисел. Верно ли, что массив является симметричным.
+
+import java.util.Scanner;
+
+public class Task4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите размер массива: ");
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Введи значения массива: ");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        boolean flag = true;
+        flag = isSimmetric(arr, flag);
+
+        if (isSimmetric(arr, flag)) {
+            System.out.println("Массив симметричный");
+        } else {
+            System.out.println("Массив не симметричный");
+        }
+
+        System.out.println("Массив выглядит так: ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + ",");
+        }
+    }
+
+    private static boolean isSimmetric(int[] arr, boolean flag) {
+        for (int i = 0; i < arr.length / 2; i++) {
+            if (arr[i] != arr[arr.length - 1 - i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
