5e7328a76d41ee91fd1e8a5b9dbb412bd3495563
MosolovIR <mosolovolosom@gmail.com>
2024-06-27 22:14:55 +0300

    Seminar 2 - 27.06.2024

--- src/main/java/org/geekbrains/Main.java
+++ src/main/java/org/geekbrains/Main.java
@@ -1,7 +1,19 @@
 package org.geekbrains;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.printf("Hello world!");
-    }
+//    public int[] twoSum(int[] nums, int target) {
+//        for (int i = 0; i < nums.length; i++) {
+//            if (nums[i] > target) {
+//                continue;
+//            } else {
+//                int firstNum = nums[i];
+//                int secondNum = Arrays.indexOf(nums, (target - firstNum));
+//                if (secondNum == -1) {
+//                    continue;
+//                } else {
+//                    return (i + secondNum);
+//                }
+//            }
+//        }
+//    }
 }
